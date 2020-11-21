5145f5864e43f27321977d9346e3b351d775ad14
SameerK16 <sameerkoleshwar@gmail.com>
2020-11-21 20:24:52 +0530

    added queue using linked list

---
+++ Queue/linkedlist.h
@@ -0,0 +1,15 @@
+#ifndef LINKEDLIST_H_INCLUDED
+#define LINKEDLIST_H_INCLUDED
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}node;
+
+int getlength(node *head);
+node* insert(node *head, int data, int pos);
+node* remove_node(node *head, int pos);
+void display(node *head);
+
+#endif // LINKEDLIST_H_INCLUDED
---
+++ Queue/queue.c
@@ -0,0 +1,63 @@
+#include "linkedlist.c"
+
+void que()
+{
+    printf(" Queue ADT using linked list\n");
+    int op, data, pos;
+    node *front = NULL, *last = NULL, *temp;
+
+    while(1)
+    {
+        printf("0.Exit  1.Enqueue  2.Dequeue  3.Display  4.Length  5.isEmpty  6.isFull :  ");
+        scanf("%d", &op);
+        switch(op)
+        {
+        case 0:
+            exit(0);
+
+        case 1:
+            printf("Enter data: ");
+            scanf("%d", &data);
+            last = enqueue(last, data);
+            if(front == NULL)
+            {
+                front = (node *)malloc(sizeof(node));
+                front = last;
+            }
+            break;
+
+        case 2:
+            front = remove_node(front, 0);
+            break;
+
+        case 3:
+            printf("Display queue: ");
+            display(front);
+            printf("\n");
+            break;
+
+        case 4:
+            printf("Length of queue: %d\n", getlength(front));
+            break;
+
+        case 5:
+            pos = (front == last) ? 1 : 0;
+            printf("isEmpty: %d", pos);
+            break;
+
+        case 6:
+            temp = (node *)malloc(sizeof(node));
+            pos = (temp == NULL) ? 1 : 0
+            printf("isFull : %d", pos);
+            break;
+
+        }
+    }
+}
+
+
+int main()
+{
+    que();
+    return 0;
+}
